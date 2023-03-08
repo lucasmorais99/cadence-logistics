b5d09454d89c0bb08443895a5f01aefa509cd4f5
Lucas Morais <44041139+lucasmorais99@users.noreply.github.com>
2023-03-07 23:06:53 -0300

    Teste

--- routes/Home/Home.test.ts
+++ routes/Home/Home.test.ts
@@ -9,29 +9,29 @@ describe("Home.vue", () => {
 
    test("should show section 1 text", () => {
       let section1 = setup.wrapper.find("section.Section-1");
-      expect(section1.text()).toContain("Here to there, in record time.");
+      expect(section1.text()).toContain("Seu produto no destino, rápido e eficiente.");
       const button = section1.find("button.Action-Button");
-      expect(button.text()).toBe("Get Started");
+      expect(button.text()).toBe("Pedir uma cotação");
    });
 
    test("should show section 2 text", () => {
       let section2 = setup.wrapper.find("section.Section-2");
-      expect(section2.text()).toContain("Cheap, affordable costs.");
+      expect(section2.text()).toContain("Barato e acessível para todos.");
       const button = section2.find("button.Action-Button");
-      expect(button.text()).toBe("See Our Pricing List");
+      expect(button.text()).toBe("Veja nossos preços");
    });
 
    test("should show section 3 text", () => {
       let section3 = setup.wrapper.find("section.Section-3");
-      expect(section3.text()).toContain("Seamless and Secure Delivery.");
+      expect(section3.text()).toContain("Entregas seguras e rastreadas.");
       const button = section3.find("button.Action-Button");
-      expect(button.text()).toBe("Learn More");
+      expect(button.text()).toBe("Ver mais");
    });
 
    test("should show section 4 text", () => {
       let section4 = setup.wrapper.find("section.Section-4");
-      expect(section4.text()).toContain("Ready to Get Started?");
+      expect(section4.text()).toContain("Pronto para começar?");
       const button = section4.find("button.Action-Button");
-      expect(button.text()).toBe("Send an Item");
+      expect(button.text()).toBe("Enviar um item");
    });
 });
